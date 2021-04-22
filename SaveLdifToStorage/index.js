9ee5767d0ac6fd0f69a067d1f860f599a4ecda03
shravya <shravya.kasturi@leanix.net>
2021-04-22 15:19:07 +0530

    changes made to accomodate topics and teams in ldif

--- SaveLdifToStorage/index.js
+++ SaveLdifToStorage/index.js
@@ -5,35 +5,175 @@
  */
 const {ContainerClient} = require("@azure/storage-blob");
 
-module.exports = async function (context, {partialResults, GetOrgTeamsData, containerSasUrl, workspaceId}) {
-    const combinedResults = partialResults.flatMap(partial => partial);
-    let existingLanguagesIds = []
-    let existingLabelsIds = []
-    let data = []
-    let partialData = []
-    combinedResults.map(repoData => {
-        [partialData, existingLanguagesIds, existingLabelsIds] = convertToLdif(repoData, existingLanguagesIds, existingLabelsIds)
-        data = [...data, ...partialData]
-    })
-    /* Creating one final LDIF to store into the blob after strigifying the object */
-    const finalLDIF = {
-        "connectorId": "github-connector",
-        "connectorType": "github-connector",
-        "connectorVersion": "1.0.0",
-        "processingDirection": "inbound",
-        "processingMode": "full",
-        "lxVersion": "1.0.0",
-        "description": "Map organisation github repos to LeanIX Fact Sheets",
-        "content": data
-    }
-    context.log('stats: partial results#', partialResults.length, 'complete partial results#',
-        partialResults.map(x => x.length).reduce((sum, c) => sum + c, 0), 'combined results#', combinedResults.length)
+const ldifHeader = {
+    "connectorId": "github-connector",
+    "connectorType": "github-connector",
+    "connectorVersion": "1.0.0",
+    "processingDirection": "inbound",
+    "processingMode": "full",
+    "lxVersion": "1.0.0",
+    "description": "Map organisation github repos to LeanIX Fact Sheets"
+}
 
+module.exports = async function (context, {partialResults, GetOrgTeamsData, containerSasUrl, workspaceId}) {
+    const contentArray = handleLdifCreation(partialResults, GetOrgTeamsData)
+    const blobName = await uploadToBlob(containerSasUrl, workspaceId, getFinalLdif(contentArray))
+    return blobName
+};
+
+/**
+ * 
+ * @param {Array} partialResults contains repository information
+ * @param {Array} orgTeamsData contains teams information
+ */
+function handleLdifCreation (partialResults, orgTeamsData) {
+    const combinedResults = partialResults.flatMap(partial => partial)
+    const reposLanguagesMap = {}
+    const reposTopicsMap = {}
+    let contentArray = []
+    for(let repoData of combinedResults) {
+
+        //maintaining a map of all languages used in the repositories
+        //we later use this map to create language content items
+        if (repoData.languages) {
+            for(let language of repoData.languages.nodes) {
+                if (!(language.id in reposLanguagesMap)) {
+                    reposLanguagesMap[language.id] = language
+                }
+            }
+        }
+
+        //maintaining a map of all repository topics used in the repositories
+        //we later use this map to create topic content items
+        if (repoData.repositoryTopics) {
+            for(let repoTopic of repoData.repositoryTopics.nodes) {
+                if (!(repoTopic.topic.id in reposTopicsMap)) {
+                    reposTopicsMap[repoTopic.topic.id] = repoTopic.topic
+                }
+            }
+        }
+
+        //pushing repository content object into the content array
+        contentArray.push(convertToRepositoryContent(repoData))
+    }
+    
+    //pushing language content objects into the content array
+    for(let langNode of Object.keys(reposLanguagesMap)) {
+        contentArray.push(convertToLanguageContent(reposLanguagesMap[langNode]))
+    }
+
+    //pushing repo topic content objects into the content array
+    for(let repoTopicNode of Object.keys(reposTopicsMap)) {
+        contentArray.push(convertToRepoTopicContent(reposTopicsMap[repoTopicNode]))
+    }
+
+    //pushing teams content objects into the content array
+    if (orgTeamsData && orgTeamsData.length) {
+        for(let teamNode of orgTeamsData.teams.nodes) {
+            contentArray.push(convertToTeamContent(teamNode))
+        }
+    }
+    return contentArray
+}
+
+/**
+ * 
+ * @param {Object} repoData contains repository info from github
+ */
+
+/* convert Repo Data to object into repository content object for final LDIF */
+function convertToRepositoryContent (repoData) {
+    let tempArr = repoData.repositoryTopics && repoData.repositoryTopics.nodes.map((node)=> 
+        node.topic.id
+    )
+    return {
+        type: "Repository",
+        id: repoData.id,
+        data: {
+            name: repoData.name,
+            url: repoData.url,
+            languages: repoData.languages && repoData.languages.nodes.map((node)=>
+                node.id
+            ),
+            topics: repoData.repositoryTopics && repoData.repositoryTopics.nodes.map(({topic})=> 
+                topic.id
+            )
+        }
+    }
+}
+
+/**
+ * 
+ * @param {Object} langData contains language info related to repository
+ */
+function convertToLanguageContent (langData) {
+    return {
+        type: 'Language',
+        id: langData.id,
+        data: {
+            name: langData.name
+        }
+    }
+}
+
+/**
+ * 
+ * @param {Object} topicData contains topic data related to repo
+ */
+function convertToRepoTopicContent (topicData) {
+    return {
+        type: 'Topic',
+        id: topicData.id,
+        data: {
+            name: topicData.name
+        }
+    }
+}
+
+/**
+ * 
+ * @param {Object} teamData contains team data as part of org
+ */
+function convertToTeamContent (teamData) {
+    return {
+        type: "Repository",
+        id: teamData.id,
+        data: {
+            name: teamData.name,
+            repositories: teamData.repositories && teamData.repositories.nodes.map((node)=>
+                node.id
+            )
+        }
+    }
+}
+
+/**
+ * 
+ * @param {Array} contentArray array containing respository info
+ * 
+ */
+function getFinalLdif (contentArray) {
+    const ldifContent = {
+        content: contentArray
+    }
+    const finalLdif = {...ldifHeader, ...ldifContent}
+    return finalLdif
+}
+
+
+/**
+ * 
+ * @param {String} containerSasUrl SAS url for the container 
+ * @param {String} workspaceId LeanIX WS ID related to the organisation
+ * @param {Object} finalLdif LDIF object containing repo and lang info
+ * 
+ */
+async function uploadToBlob (containerSasUrl, workspaceId, finalLdif) {
     const containerClient = new ContainerClient(containerSasUrl);
 
     const blobName = `${workspaceId}-${Date.now()}.json`;
     const blockBlobClient = containerClient.getBlockBlobClient(blobName);
-    const finalLdifData = JSON.stringify(finalLDIF);
+    const finalLdifData = JSON.stringify(finalLdif);
 
     try {
         /* Uploading the final stringified json object as a block blob */
@@ -42,48 +182,4 @@ module.exports = async function (context, {partialResults, GetOrgTeamsData, cont
         return '__UPLOAD_FAILED';
     }
     return blobName;
-};
-
-/* covertToLdif function take a single repoData object and extracts 3 types of 
-   content objects, Repository, Language and Label. To avoid repetition, already
-   considered languages and labels are maintained as arrays and updated accordingly */
-function convertToLdif(repoData, existingLanguagesIds, existingLabelsIds) {
-    const ldif = {
-        type: "Repository",
-        id: repoData.id,
-        data: {
-            name: repoData.name,
-            description: repoData.description,
-            createdAt: repoData.createdAt,
-            languages: repoData.languages && repoData.languages.edges.map(({node})=>
-                node.id
-            ),
-            labels: repoData.labels && repoData.labels.edges.map(({node})=>
-                node.id
-            )
-        }
-    }
-    langLdifs = repoData.languages && repoData.languages.edges.filter(({node})=> !existingLanguagesIds.includes(node.id)).map(({node}) => {
-            existingLanguagesIds.push(node.id)
-            return {
-                type: 'Language',
-                id: node.id,
-                data: {
-                    name: node.name
-                }
-            }
-    })
-    labelLdifs = repoData.labels && repoData.labels.edges.filter(({node})=> !existingLabelsIds.includes(node.id)).map(({node}) => {
-            existingLabelsIds.push(node.id)
-            return {
-                type: 'Label',
-                id: node.id,
-                data: {
-                    name: node.name,
-                    color: node.color,
-                    description: node.description
-                }
-            }
-    })
-    return [[ldif, ...langLdifs, ...labelLdifs], existingLanguagesIds, existingLabelsIds];
 }
