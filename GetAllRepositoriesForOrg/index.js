c89628b98dc093c349e5e1ad03832fdc453882ab
Aravind Metku <64901333+aravindmetku@users.noreply.github.com>
2021-04-27 14:02:13 +0530

    Feature/cid 86/fetch GitHub data (#3)
    * cid-86 fetch team data with all repositories.
    Initially getting 50 repos and check if there are more then fetch all repos separately
    
    * get repo info by ids, pagination for languages
    
    Co-authored-by: Meisi Zhan <meisi.zhan@codecentric.de>
    Co-authored-by: shravya <shravya.kasturi@leanix.net>

--- GetAllRepositoriesForOrg/index.js
+++ GetAllRepositoriesForOrg/index.js
@@ -5,32 +5,54 @@
  */
 const {graphql} = require("@octokit/graphql");
 
-module.exports = async function (context, {orgName, ghToken}) {
-    // retrieves all ids of an organisation
-    const data = await graphql({
+async function getRepositoriesIds(graphqlClient, {queryString, pageCount, cursor}) {
+    const data = await graphqlClient({
         query: `
-            query getOrgRepositories($queryString: String!) {
-              search(query: $queryString, type: REPOSITORY, first: 5) {
-                repositoryCount
-                edges {
-                  node {
-                    ... on Repository {
-                      id
-                    }
+            query getOrgRepositories($queryString: String!, $pageCount: Int!, $cursor: String) {
+              organization(login: $queryString) {
+                repositories(first: $pageCount, after: $cursor) {
+                  pageInfo {
+                    endCursor
+                    hasNextPage
                   }
-                }
-                pageInfo {
-                  hasNextPage
-                  startCursor
+                  nodes {
+                    id
+                  }
                 }
               }
             }
         `,
-        queryString: `org:${orgName}`,
+        queryString,
+        pageCount,
+        cursor
+    });
+
+    return {
+        ids: data.organization.repositories.nodes.map(e => e.id),
+        pageInfo: data.organization.repositories.pageInfo
+    };
+}
+
+async function getAllRepositoryIds(graphqlClient, queryString) {
+    let cursor = null;
+    let finalResult = [];
+
+    do {
+        var {ids, pageInfo} = await getRepositoriesIds(graphqlClient, {queryString, pageCount: 100, cursor});
+        finalResult = finalResult.concat(ids);
+        cursor = pageInfo.endCursor;
+    } while (pageInfo.hasNextPage);
+    return finalResult;
+}
+
+module.exports = async function (context, {orgName}) {
+    // retrieves all ids of an organisation
+    const graphqlClient = graphql.defaults({
         headers: {
-            authorization: `token ${ghToken}`,
+            authorization: `token ${process.env['ghToken']}`,
         },
     });
-    const finalData = data.search.edges.map(e => e.node.id)
-    context.done(null, finalData);
+
+    const finalResult = await getAllRepositoryIds(graphqlClient, orgName);
+    context.done(null, finalResult);
 };
--- GetSubReposData/index.js
+++ GetSubReposData/index.js
@@ -9,20 +9,111 @@
  *   function app in Kudu
  */
 
+const {graphql} = require("@octokit/graphql");
+
 module.exports = async function (context, repoIds) {
-    context.log('partial act func called', repoIds);
-    const data = getReposData(repoIds).map(convertToLdif);
-    return data;
+    const graphqlClient = graphql.defaults({
+        headers: {
+            authorization: `token ${process.env['ghToken']}`,
+        },
+    });
+    return await getReposData(graphqlClient, repoIds)
 };
 
-function getReposData(repoIds) {
-    const data = [];
-    return repoIds;
+async function getReposData(graphqlClient, repoIds) {
+    const initialLanguagePageSize = 10;
+    const data = await graphqlClient({
+        query: `
+            query getReposData($repoIds:[ID!]!, $languagePageCount: Int!){
+                nodes(ids: $repoIds){
+                    id
+                    ... on Repository {
+                        name
+                        url
+                        description
+                        languages(first: $languagePageCount) {
+                            pageInfo {
+                                endCursor
+                                hasNextPage
+                            }
+                            edges{
+                                size
+                                node{
+                                    id
+                                    name
+                                }
+                            }
+                        }
+                        repositoryTopics(first: 10) {
+                            nodes {
+                                topic {
+                                    id
+                                    name
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+        `,
+        repoIds,
+        languagePageCount: initialLanguagePageSize
+    });
+
+    let repoInfos = data.nodes;
+    for (let repoInfo of repoInfos) {
+        if (repoInfo.languages.pageInfo.hasNextPage) {
+            repoInfo.languages.nodes = await getAllLanguagesForRepo(graphqlClient, repoInfo)
+        }
+    }
+
+    return repoInfos;
 }
 
-function convertToLdif(repoData) {
-    const ldif = {
-        id: repoData
+async function getAllLanguagesForRepo(graphqlClient, repoInfo) {
+    let languageCursor = null;
+    let finalResult = [];
+
+    do {
+        var {languages, pageInfo} = await getPagedLanguages(graphqlClient, {repoId: repoInfo.id, cursor: languageCursor});
+        finalResult = finalResult.concat(languages);
+        languageCursor = pageInfo.endCursor;
+    } while (pageInfo.hasNextPage);
+
+    return finalResult;
+}
+
+async function getPagedLanguages(graphqlClient, {repoId, cursor}) {
+    const languagePageSize = 100;
+    const data = await graphqlClient({
+        query: `
+            query getLanguagesForRepo($repoId: ID!, $pageCount: Int!, $cursor: String) {
+                node(id: $repoId) {
+                    id
+                    ... on Repository {
+                        languages(first: $pageCount, after: $cursor) {
+                            pageInfo {
+                                endCursor
+                                hasNextPage
+                            }
+                            edges{
+                                size
+                                node{
+                                    id
+                                    name
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        `,
+        repoId,
+        cursor,
+        pageCount: languagePageSize
+    });
+    return {
+        languages: data.node.languages.nodes,
+        pageInfo: data.node.languages.pageInfo
     }
-    return ldif;
 }
