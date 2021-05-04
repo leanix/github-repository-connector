f4e0280ab3c4dbe1a219daa38614d50a391fd74a
aravindmetku <aravind.metku@leanix.net>
2021-05-04 21:35:48 +0530

    cid-114 Modified output response of http trigger for camunda AzureFunctionCall task

--- HttpStartOrgScan/index.js
+++ HttpStartOrgScan/index.js
@@ -1,10 +1,49 @@
 ﻿const df = require("durable-functions");
 
 module.exports = async function (context, req) {
-    const client = df.getClient(context);
-    const instanceId = await client.startNew("GithubRepoScanOrchestrator", undefined, req.body);
+    const client = df.getClient(context)
+    const params = req.body;
+    let azureCallId = params.azureCallId;
 
-    context.log(`Started orchestration with ID = '${instanceId}'.`);
+    try {
+        if (!azureCallId) {
+            azureCallId = await client.startNew("GithubRepoScanOrchestrator", undefined, params.customConfiguration);
+            context.log(`Started orchestration with ID = '${azureCallId}'.`);
+        }
 
-    return client.createCheckStatusResponse(context.bindingData.req, instanceId);
+        const status = await client.getStatus(azureCallId);
+        return getCustomStatusResponse(status, context, azureCallId);
+    } catch (e) {
+        context.log(`Exception caught: ${e}`)
+        return buildResponseBody("?", "FAILED", {"message": `Unknown exception caught: ${e}`}, 500)
+    }
 };
+
+function getCustomStatusResponse(status, context, azure_call_id) {
+    if (status.runtimeStatus === df.OrchestrationRuntimeStatus.Completed) {
+        const data = status.output
+        context.log('Orchestrator reached status {status.runtimeStatus} - returning \'FINISHED\' response')
+        return buildResponseBody(azure_call_id, "FINISHED", data, 200)
+    } else if (status.runtimeStatus === df.OrchestrationRuntimeStatus.Pending ||
+        status.runtimeStatus === df.OrchestrationRuntimeStatus.Running ||
+        status.runtimeStatus === df.OrchestrationRuntimeStatus.ContinuedAsNew) {
+        // orchestrator still in progress - construct IN_PROGRESS response
+        context.log('Orchestrator reached status {status.runtimeStatus} - returning \'IN_PROGRESS\' response')
+        return buildResponseBody(azure_call_id, "IN_PROGRESS", status, 200)
+    } else {
+        //orchestrator reached other (error) state - construct FAILED response
+        context.log("Orchestrator reached status {status.runtime_status} - returning 'FAILED' response")
+        return buildResponseBody(azure_call_id, "FAILED", status, 200)
+    }
+}
+
+function buildResponseBody(azure_call_id, status, data, status_code) {
+    return {
+        status: status_code,
+        body: {
+            "azureCallId": azure_call_id,
+            "status": status,
+            "data": data
+        },
+    }
+}
