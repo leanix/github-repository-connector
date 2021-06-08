6a90d313951e07ef4614336aef0c3142a7879a4a
Aravind Metku <64901333+aravindmetku@users.noreply.github.com>
2021-06-08 14:21:07 +0530

    CID-133 Added capability to accept encrypted GH token

--- GetReposVisibilityData/index.js
+++ GetReposVisibilityData/index.js
@@ -1,18 +1,9 @@
-/*
- * This function is not intended to be invoked directly. Instead it will be
- * triggered by an orchestrator function.
- * 
- * Before running this sample, please:
- * - create a Durable orchestration function
- * - create a Durable HTTP starter function
- * - run 'npm install durable-functions' from the wwwroot folder of your
- *   function app in Kudu
- */
 const {graphql} = require("@octokit/graphql");
 
 /**
- * 
- * @param {Array} repoNodes 
+ *
+ * @param {Array} repoNodes
+ * @param {String} visibilityType
  */
 
 function getRepoIdsVisibilityMap(repoNodes, visibilityType) {
---
+++ GithubRepoScanOrchestrator/helper.js
@@ -0,0 +1,13 @@
+const CryptoJS = require("crypto-js");
+
+module.exports = {
+    decryptGHToken: function decrypt(encrypted) {
+        const pass = process.env['LX_ENCRYPTION_PASSPHRASE'];
+        try {
+            const decrypted = CryptoJS.AES.decrypt(encrypted, pass);
+            return CryptoJS.enc.Utf8.stringify(decrypted).trim();
+        } catch (e) {
+            throw new Error("Failed to correctly decrypt github token");
+        }
+    }
+}
