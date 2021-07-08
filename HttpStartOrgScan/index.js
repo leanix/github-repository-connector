671249af18522bd46f7bf5450af64f2f027341f5
aravindmetku <aravind.metku@leanix.net>
2021-07-08 18:35:10 +0530

    CID-175 Modified http start client function to return inprogress status as IHub doc changed

--- HttpStartOrgScan/index.js
+++ HttpStartOrgScan/index.js
@@ -1,13 +1,15 @@
 ﻿const df = require("durable-functions");
+const {iHubStatus} = require("../GithubRepoScanOrchestrator/helper");
 
 module.exports = async function (context, req) {
     const client = df.getClient(context);
     const input = req.body;
+
     const instanceId = await client.startNew("GithubRepoScanOrchestrator", undefined, input);
 
     context.log(`Started orchestration with ID = '${instanceId}'.`);
 
-    return buildResponseBody({runId: input.runId, status: "STARTED"})
+    return buildResponseBody({runId: input.runId, status: iHubStatus.IN_PROGRESS})
 };
 
 function buildResponseBody(data) {
