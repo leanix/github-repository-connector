fc307fa6e8ea3872767c5623d8d24465f3aeb004
Frank Poschner <frank.poschner@codecentric.de>
2021-08-12 13:00:28 +0200

    reverting changes that were only based on formatting

--- SaveLdifToStorage/index.js
+++ SaveLdifToStorage/index.js
@@ -1,28 +1,21 @@
 ﻿/*
  * Handles LDIF storage
  */
-const { BlobClient, AnonymousCredential } = require("@azure/storage-blob");
+const {BlobClient, AnonymousCredential} = require("@azure/storage-blob");
 
 const ldifHeader = {
-  connectorId: "github-connector",
-  connectorType: "github-connector",
-  connectorVersion: "1.0.0",
-  processingDirection: "inbound",
-  processingMode: "full",
-  lxVersion: "1.0.0",
-  description: "Map organisation github repos to LeanIX Fact Sheets",
-};
+    "connectorId": "github-connector",
+    "connectorType": "github-connector",
+    "connectorVersion": "1.0.0",
+    "processingDirection": "inbound",
+    "processingMode": "full",
+    "lxVersion": "1.0.0",
+    "description": "Map organisation github repos to LeanIX Fact Sheets"
+}
 
-module.exports = async function (
-  context,
-  { partialResults, teamResults, repoIdsVisibilityMap, blobStorageSasUrl }
-) {
-  const contentArray = handleLdifCreation(
-    partialResults,
-    teamResults,
-    repoIdsVisibilityMap
-  );
-  return await uploadToBlob(getFinalLdif(contentArray), blobStorageSasUrl);
+module.exports = async function (context, {partialResults, teamResults, repoIdsVisibilityMap, blobStorageSasUrl}) {
+    const contentArray = handleLdifCreation(partialResults, teamResults, repoIdsVisibilityMap)
+    return await uploadToBlob(getFinalLdif(contentArray), blobStorageSasUrl)
 };
 
 /**
@@ -31,43 +24,38 @@ module.exports = async function (
  * @param {Array} orgTeamsData contains teams information
  * @param {Object} repoIdsVisibilityMap contains repo id and it's visibility map
  */
-function handleLdifCreation(
-  partialResults,
-  orgTeamsData,
-  repoIdsVisibilityMap
-) {
-  const combinedResults = partialResults.flatMap((partial) => partial);
-  const reposLanguagesMap = {};
-  const reposTopicsMap = {};
-  let contentArray = [];
-  for (let repoData of combinedResults) {
-    for (let language of repoData.languages.edges) {
-      reposLanguagesMap[language.node.id] = language.node;
+function handleLdifCreation(partialResults, orgTeamsData, repoIdsVisibilityMap) {
+    const combinedResults = partialResults.flatMap(partial => partial)
+    const reposLanguagesMap = {}
+    const reposTopicsMap = {}
+    let contentArray = []
+    for (let repoData of combinedResults) {
+
+        for (let language of repoData.languages.edges) {
+            reposLanguagesMap[language.node.id] = language.node
+        }
+
+        for (let repoTopic of repoData.repositoryTopics.nodes) {
+            reposTopicsMap[repoTopic.topic.id] = repoTopic.topic
+        }
+
+        repoData.visibility = repoIdsVisibilityMap[repoData.id] ? repoIdsVisibilityMap[repoData.id] : null
+        contentArray.push(convertToRepositoryContent(repoData))
     }
 
-    for (let repoTopic of repoData.repositoryTopics.nodes) {
-      reposTopicsMap[repoTopic.topic.id] = repoTopic.topic;
+    for (let langNode of Object.values(reposLanguagesMap)) {
+        contentArray.push(convertToLanguageContent(langNode))
     }
 
-    repoData.visibility = repoIdsVisibilityMap[repoData.id]
-      ? repoIdsVisibilityMap[repoData.id]
-      : null;
-    contentArray.push(convertToRepositoryContent(repoData));
-  }
+    for (let repoTopicNode of Object.values(reposTopicsMap)) {
+        contentArray.push(convertToRepoTopicContent(repoTopicNode))
+    }
 
-  for (let langNode of Object.values(reposLanguagesMap)) {
-    contentArray.push(convertToLanguageContent(langNode));
-  }
+    for (let teamNode of orgTeamsData) {
+        contentArray.push(convertToTeamContent(teamNode))
+    }
 
-  for (let repoTopicNode of Object.values(reposTopicsMap)) {
-    contentArray.push(convertToRepoTopicContent(repoTopicNode));
-  }
-
-  for (let teamNode of orgTeamsData) {
-    contentArray.push(convertToTeamContent(teamNode));
-  }
-
-  return contentArray;
+    return contentArray
 }
 
 /**
@@ -81,23 +69,25 @@ function handleLdifCreation(
 */
 
 function convertToRepositoryContent(repoData) {
-  return {
-    type: "Repository",
-    id: repoData.id,
-    data: {
-      name: repoData.name,
-      url: repoData.url,
-      description: repoData.description,
-      languages: repoData.languages.edges.map(({ size, node }) => {
-        return {
-          langId: node.id,
-          size: (size / 1000).toFixed(2),
-        };
-      }),
-      topics: repoData.repositoryTopics.nodes.map(({ topic }) => topic.id),
-      repoVisibility: repoData.visibility,
-    },
-  };
+    return {
+        type: "Repository",
+        id: repoData.id,
+        data: {
+            name: repoData.name,
+            url: repoData.url,
+            description: repoData.description,
+            languages: repoData.languages.edges.map(({size, node}) => {
+                return {
+                    langId: node.id,
+                    size: (size / 1000).toFixed(2)
+                }
+            }),
+            topics: repoData.repositoryTopics.nodes.map(({topic}) =>
+                topic.id
+            ),
+            repoVisibility: repoData.visibility
+        }
+    }
 }
 
 /**
@@ -105,13 +95,13 @@ function convertToRepositoryContent(repoData) {
  * @param {Object} langData contains language info related to repository
  */
 function convertToLanguageContent(langData) {
-  return {
-    type: "Language",
-    id: langData.id,
-    data: {
-      name: langData.name,
-    },
-  };
+    return {
+        type: 'Language',
+        id: langData.id,
+        data: {
+            name: langData.name
+        }
+    }
 }
 
 /**
@@ -119,13 +109,13 @@ function convertToLanguageContent(langData) {
  * @param {Object} topicData contains topic data related to repo
  */
 function convertToRepoTopicContent(topicData) {
-  return {
-    type: "Topic",
-    id: topicData.id,
-    data: {
-      name: topicData.name,
-    },
-  };
+    return {
+        type: 'Topic',
+        id: topicData.id,
+        data: {
+            name: topicData.name
+        }
+    }
 }
 
 /**
@@ -133,15 +123,17 @@ function convertToRepoTopicContent(topicData) {
  * @param {Object} teamData contains team data as part of org
  */
 function convertToTeamContent(teamData) {
-  return {
-    type: "Team",
-    id: teamData.id,
-    data: {
-      name: teamData.name,
-      parent: teamData.parentTeam ? teamData.parentTeam.id : null,
-      repositories: teamData.repositories.nodes.map((node) => node.id),
-    },
-  };
+    return {
+        type: "Team",
+        id: teamData.id,
+        data: {
+            name: teamData.name,
+            parent: teamData.parentTeam ? teamData.parentTeam.id : null,
+            repositories: teamData.repositories.nodes.map((node) =>
+                node.id
+            )
+        }
+    }
 }
 
 /**
@@ -150,10 +142,10 @@ function convertToTeamContent(teamData) {
  *
  */
 function getFinalLdif(contentArray) {
-  const ldifContent = {
-    content: contentArray,
-  };
-  return { ...ldifHeader, ...ldifContent };
+    const ldifContent = {
+        content: contentArray
+    }
+    return {...ldifHeader, ...ldifContent};
 }
 
 /**
@@ -162,10 +154,7 @@ function getFinalLdif(contentArray) {
  * @param {String} blobStorageSasUrl SAS Url generated and received from Integration Hub
  */
 async function uploadToBlob(finalLdif, blobStorageSasUrl) {
-  const blockBlobClient = new BlobClient(
-    blobStorageSasUrl,
-    new AnonymousCredential()
-  ).getBlockBlobClient();
-  const finalLdifData = JSON.stringify(finalLdif);
-  await blockBlobClient.upload(finalLdifData, Buffer.byteLength(finalLdifData));
+    const blockBlobClient = new BlobClient(blobStorageSasUrl, new AnonymousCredential()).getBlockBlobClient();
+    const finalLdifData = JSON.stringify(finalLdif);
+    await blockBlobClient.upload(finalLdifData, Buffer.byteLength(finalLdifData))
 }
