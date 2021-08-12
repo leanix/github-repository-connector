3d2f21ca74a0cfd21ca8633ed58ea19310232dae
Frank Poschner <frank.poschner@codecentric.de>
2021-08-12 16:34:18 +0200

    checking the regex and throwing an error in case it is not valid.

--- GetAllRepositoriesForOrg/helper.js
+++ GetAllRepositoriesForOrg/helper.js
@@ -1,9 +1,13 @@
 module.exports = {
-  checkRegexExcludeList: function (regexExcludeList) {
-    if (regexExcludeList) {
-      let regexExcludeListArray = regexExcludeList.split(",");
-      return regexExcludeListArray.map((regexString) => new RegExp(regexString));
-    }
-    return [];
-  },
+	checkRegexExcludeList: function (regexExcludeList) {
+		if (regexExcludeList) {
+			let regexExcludeListArray = regexExcludeList.split(',');
+			try {
+				return regexExcludeListArray.map((regexString) => new RegExp(regexString));
+			} catch (error) {
+				throw new Error('A regular expression provided in the input field repoNamesExcludeList is invalid. Please check your input!');
+			}
+		}
+		return [];
+	}
 };
